f64dd2eca8f2dbc8cbd52d1ecd8a44cc5f6067a8
CDiangell-dei <cield@example.com>
2026-08-05 23:49:46 -0400

    feat: use vite-plugin-singlefile to bundle entire app into a single index.html file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/RPG-Plataform/',
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   build: {
     outDir: 'docs',
   }
